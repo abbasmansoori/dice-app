21d6e03263cc6ed39ec91a7b38cb7b0e11190ffb
abbasmansoori <abbas.mansoori@hotmail.com>
2021-11-24 01:20:27 +0100

    added animation to dice

--- dice-app/src/RollDice.js
+++ dice-app/src/RollDice.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Die from "./Die";
 import "./RollDice.css";
+import "./Die.css"
 
 class RollDice extends Component {
   static defaultProps = {
@@ -8,7 +9,7 @@ class RollDice extends Component {
   };
   constructor(props) {
     super(props);
-    this.state = { die1: "one", die2: "one" };
+    this.state = { die1: "one", die2: "one", rolling: false };
     this.roll = this.roll.bind(this);
   }
 
@@ -17,17 +18,23 @@ class RollDice extends Component {
       this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
     const newDie2 =
       this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
-    this.setState({ die1: newDie1, die2: newDie2 });
+    this.setState({ die1: newDie1, die2: newDie2, rolling: true });
+
+    setTimeout(() => {
+      this.setState({ rolling: false });
+    }, 1000);
   }
 
   render() {
     return (
       <div className="RollDice">
-      <div className="RollDice-container">
-        <Die face={this.state.die1} />
-        <Die face={this.state.die2} />
+        <div className="RollDice-container">
+          <Die face={this.state.die1} rolling={this.state.rolling} />
+          <Die face={this.state.die2} rolling={this.state.rolling} />
         </div>
-        <button onClick={this.roll}>Roll Dice!</button>
+        <button  onClick={this.roll} disabled={this.state.rolling}>
+          {this.props.rolling ? "Rolling..." : "Roll Dice"} 
+        </button>
       </div>
     );
   }
